954f06c6f596d8fcb0b6cb3df0489b210195d10d
singhHarcharan <harcharansingh198400@gmail.com>
2025-05-02 23:38:49 +0530

    Setup of Backend folder

---
+++ backend/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+function sum(a, b) {
+    return a + b;
+}
+let result = sum(10, 20);
+console.log("Sum of two numbers is ", result);
---
+++ backend/src/index.ts
@@ -0,0 +1,6 @@
+function sum(a: number, b: number): number {
+    return a + b;
+}
+
+let result: number = sum(10, 20);
+console.log("Sum of two numbers is ", result);
