c84b5d8c3d9d291a828323774848311512fb32e5
singhHarcharan <harcharansingh198400@gmail.com>
2025-05-11 10:43:14 +0530

    fixed some dependencies issue, but need to update and add more.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { SavedRecruitersProvider } from './context/SavedRecruitersContext';
 import { SelectedRecruitersProvider } from './context/SelectedRecruitersContext';
